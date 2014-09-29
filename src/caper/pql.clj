8a450f79005946d2ca0c0acc62a4b11cf3fa3c74
falix.zhu <falix.zhu@netis.com.cn>
2014-09-29 12:00:04 +0800

    update

--- src/caper/pql.clj
+++ src/caper/pql.clj
@@ -1,65 +1,86 @@
-(ns clopcap.pql
-  :require [clj-antlr.core :as antlr]
-  :use [clojure.string :only join])
+(ns caper.pql
+  (:require [clj-antlr.core :as antlr])
+  (:use [clojure.string :only [join]]))
+
+(def ^{:private true
+       :dynamic true}
+  commands (ref {}))
+
+(def ^{:private true
+       :dynamic true}
+  comparisons {:> :>= := :< :<= :!=})
 
 (defmacro def-command
-  [cmd & body]
-  ())
+  [cmd grammar & body]
+  `(dosync
+    (alter commands assoc
+           (keyword '~cmd)
+           {:grammar ~grammar
+            :func (fn ~body)})))
 
 (defn do-command
-  [data & params]
-  ())
+  [comd & params]
+  (apply
+   (:func ((keyword comd) @commands)) params))
 
 (def-command find
-  '[WHERE+ expression]
+  "expression"
   [expression data]
-  ())
+  (println expression))
 
 (def-command dump
-  '[STRING WHERE expression]
-  [data file-name expression]
-  (write-pcap file-name (do-command data expression)))
+  "STRING WHERE expression"
+  [expression data]
+  (let [[dump-file sub-ex] expression]
+    (do-command :find sub-ex data)))
 
 (defn join-keys
   [separator coll]
   (join separator
         (map #(name (first %)) coll)))
 
-(defn anltr-rule
+(defn- antlr-commands []
+  (apply concat
+         (map #(vector
+                (first %)
+                (str (name (first %)) " " (:grammar (second %))))
+              @commands)))
+
+
+(defn antlr-rule
   [& options]
   (def ^:dynamic *parser*
-    (anltr/parser
+    (antlr/parser
      (apply str
             (map #(if (keyword? %)
                     (str (name %) ":")
                     (str % ";\n"))
                  (apply concat options))))))
 
-(anltr-rule ["grammar Pql"]
-            [:command       "COMMAND expression"
-             :expression    "or+"
+
+(antlr-rule ["grammar Pql"]
+            (antlr-commands)
+            [:expression    "or+"
              :or            "and ( OR and )*"
              :and           "atom ( AND atom )*"
              :atom          "condition | PBR expression NBR"
-             :condition     "(VAR|pld) COMP (STRING|INT|list)"
-             :list          "'[' (STRING|INT)* ']'"
-             :pld           "VAR '[' INT ':' INT ']'"]
-            [:COMMAND       (join-keys "|" *commands*)
-             :OR            "'or'  -> channel(HIDDEN)"
+             :condition     "VAR COMP (STRING|INT)"]
+            [:OR            "'or'  -> channel(HIDDEN)"
              :AND           "'and' -> channel(HIDDEN)"
              :PBR           "'('   -> channel(HIDDEN)"
              :NBR           "')'   -> channel(HIDDEN)"
+             :STRING        "'\"' (('A'..'Z'|'a'..'z'|'0'..'9'|'.'|'_') +) '\"'"
              :INT           "('0'..'9')+"
              :VAR           "('a'..'z'|'A'..'Z'|'0'..'9'|'.'|'_')+"
-             :COMP          (join-keys "|" *comparisons*)
-             :STRING        "'\"' (('A'..'Z'|'a'..'z') +) '\"'"
-             :WS            "[ \n\r\t\,] -> channel(HIDDEN)"])
-
+             :COMP          (join-keys "|" comparisons)
+             :WS            "[" "\\n\\r\\t] -> channel(HIDDEN)"])
 
 
 (defn pql-parse
   [query]
-  (*parser* query))
+  (println (*parser* query)))
+
+(pql-parse "find x > 5")
 
 (defn pql-query
   [data query]
